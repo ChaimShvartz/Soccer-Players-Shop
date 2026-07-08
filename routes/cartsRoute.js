f5bc71d382bfb4d31438abc3e3c4d2f989c92417
Chaim <c0507634153@gmail.com>
2026-07-08 23:11:07 +0300

    Add carts router with non-implemented endpoints

---
+++ routes/cartsRoute.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {});
+router.post("/items", (req, res) => {});
+router.delete("/items/:productId", (req, res) => {});
+
+export default router;
