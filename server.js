05e1a3842933f13bf470b83687b9411779015aa4
Chaim <c0507634153@gmail.com>
2026-07-08 23:14:01 +0300

    Add basic server includes routers, middlewares and not found handler

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import "dotenv/config";
+import orderRouter from "./routes/ordersRoute.js";
+import cartsRouter from "./routes/cartsRoute.js";
+import generalRouter from "./routes/generalRoute.js";
+
+const server = express();
+
+server.use(express.json());
+server.use((req, res, next) => {
+    console.log(req.url, req.method);
+    next();
+});
+
+server.use("/carts", cartsRouter);
+server.use("/orders", orderRouter);
+server.use(generalRouter);
+
+server.use((req, res) => {
+    res.status(404).json({ success: false, message: "Path not found" });
+});
+
+server.listen(process.env.PORT, () =>
+    console.log(`I'm listening on port ${process.env.PORT}...`),
+);
