5a57581e769ab8030abe51f5e6fab9678ac0d638
Chaim <c0507634153@gmail.com>
2026-07-09 09:40:32 +0300

    Add opening message and health checking endpoints on generalRoute

--- routes/generalRoute.js
+++ routes/generalRoute.js
@@ -2,9 +2,19 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {});
-router.get("/health", (req, res) => {});
+router.get("/", (req, res) => {
+    res.json({
+        success: true,
+        data: "Welcome to FirstEleven.Buy and sign professional football talent instantly.",
+    });
+});
+
+router.get("/health", (req, res) => {
+    res.json({success: true, data:"OK"})
+});
+
 router.get("/products", (req, res) => {});
+
 router.get("/account/balance", (req, res) => {});
 
 export default router;
